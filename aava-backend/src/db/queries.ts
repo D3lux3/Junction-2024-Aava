03d988554ba7c8dac5685456bad80aea848945e5
saavajuu <juuso.saavalainen@helsinki.fi>
2024-11-09 23:41:38 +0200

    magic query

--- aava-backend/src/db/queries.ts
+++ aava-backend/src/db/queries.ts
@@ -3,11 +3,18 @@ SELECT
     c.id AS company_id,
     SQRT(
         SUM(POWER(wbc.weight - wb.weight, 2))
-    ) AS distance
+    ) AS distance,
+    JSON_AGG(
+        JSON_BUILD_OBJECT(
+            'name', wbc.name,
+            'applicant_weight', wbc.weight,
+            'company_weight', wb.weight
+        )
+    ) AS weights
 FROM
     applicants a
 JOIN applicantwellbeingvalues wbc ON a.id = wbc.applicant_id
-JOIN companywellbeingvalues wb ON wbc.name = wb.name
+JOIN companywellbeingvalues wb ON wbc.name = wb.name AND wb.company_id = c.id
 JOIN companies c ON c.id = wb.company_id
 WHERE
     a.id = $1
