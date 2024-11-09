9e36aa2f1c49037a0c2556eda654da1ca69ee725
saavajuu <juuso.saavalainen@helsinki.fi>
2024-11-09 23:50:25 +0200

    just kidding here comes the real magix

--- aava-backend/src/db/queries.ts
+++ aava-backend/src/db/queries.ts
@@ -14,12 +14,12 @@ SELECT
 FROM
     applicants a
 JOIN applicantwellbeingvalues wbc ON a.id = wbc.applicant_id
-JOIN companywellbeingvalues wb ON wbc.name = wb.name AND wb.company_id = c.id
+JOIN companywellbeingvalues wb ON wbc.name = wb.name
 JOIN companies c ON c.id = wb.company_id
 WHERE
-    a.id = $1
+    a.id = '123'
 GROUP BY
     c.id
 ORDER BY
-    distance;
+    distance
 `;
