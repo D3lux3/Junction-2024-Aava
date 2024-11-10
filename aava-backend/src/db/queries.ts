ef13793b39d9e1fafa700f0de7df8ee1eb72490d
saavajuu <juuso.saavalainen@helsinki.fi>
2024-11-10 07:43:10 +0200

    magic night

--- aava-backend/src/db/queries.ts
+++ aava-backend/src/db/queries.ts
@@ -17,7 +17,7 @@ JOIN applicantwellbeingvalues wbc ON a.id = wbc.applicant_id
 JOIN companywellbeingvalues wb ON wbc.name = wb.name
 JOIN companies c ON c.id = wb.company_id
 WHERE
-    a.id = '123'
+    a.id = $1
 GROUP BY
     c.id
 ORDER BY
